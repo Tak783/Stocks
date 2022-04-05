91750a83da6f5b418d43a3f6e471322d8845909c
Tak Mazarura <takmazarura@hotmail.co.uk>
2022-04-05 19:34:29 +0100

    P1-00004: Removed unnecessary files and organised project into folders

---
+++ Layers/Presentation/StocksiOSApp/StocksiOSApp/App/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  StocksiOSApp
+//
+//  Created by Tak Mazarura on 05/04/2022.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
