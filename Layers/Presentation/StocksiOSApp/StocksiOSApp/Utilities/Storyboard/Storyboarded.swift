5df562dd2a4ce492a20d397d68ea2d38d2ea8503
Tak Mazarura <takmazarura@hotmail.co.uk>
2022-04-06 22:05:02 +0100

    P1-00005: Added Storyboarded protocol and ViewController for easy instantiation of view controllers

---
+++ Layers/Presentation/StocksiOSApp/StocksiOSApp/Utilities/Storyboard/Storyboarded.swift
@@ -0,0 +1,12 @@
+//
+//  Storyboarded.swift
+//  StocksiOSApp
+//
+//  Created by Tak Mazarura on 06/04/2022.
+//
+
+import Foundation
+
+protocol Storyboarded {
+    static func instantiate() -> Self
+}
